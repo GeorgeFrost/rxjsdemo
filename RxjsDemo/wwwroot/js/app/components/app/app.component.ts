a3913d71f37db6de94bab448b482fbb30a8b1673
unknown <george.frost@esendex.com>
2016-04-20 01:22:12 +0100

    Changed to ASPNet5 project with gulp dependency

---
+++ RxjsDemo/wwwroot/js/app/components/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from 'angular2/core';
+
+import {DashboardComponent} from '../dashboard/dashboard.component'
+
+@Component({
+    selector: 'app',
+    templateUrl: 'js/app/components/app/app.html',
+    directives: [DashboardComponent]
+})
+
+export class AppComponent {}
---
+++ RxjsDemo/wwwroot/js/app/main.ts
@@ -0,0 +1,11 @@
+﻿///<reference path="../../../node_modules/angular2/typings/browser.d.ts"/> 
+///<reference path="../../../typings/browser.d.ts"/> 
+
+import {AppComponent} from './components/app/app.component';
+//import {DashboardComponent} from './components/dashboard/dashboard.component';
+
+//import {DashboardService} from './services/dashboardService';
+
+import {bootstrap}    from 'angular2/platform/browser';
+
+bootstrap(AppComponent);
